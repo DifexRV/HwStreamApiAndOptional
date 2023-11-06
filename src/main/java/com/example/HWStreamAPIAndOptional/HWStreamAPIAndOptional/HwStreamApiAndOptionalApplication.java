594e88cdcca0127433e77fbc95d3920547135044
Roman <roOmkin@gmail.com>
2023-11-06 21:06:54 +0700

    Мастер ветка - Stream API и Optional.

---
+++ src/main/java/com/example/HWStreamAPIAndOptional/HWStreamAPIAndOptional/HwStreamApiAndOptionalApplication.java
@@ -0,0 +1,13 @@
+package com.example.HWStreamAPIAndOptional.HWStreamAPIAndOptional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwStreamApiAndOptionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwStreamApiAndOptionalApplication.class, args);
+	}
+
+}
